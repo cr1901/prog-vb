c0d1980d278bb902369c2d5e01653b3252dae8d8
William D. Jones <thor0505@comcast.net>
2019-02-28 09:52:29 -0500

    Write FlashBoy detection code.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,66 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate failure;
+extern crate hidapi;
+extern crate exitfailure;
+
+use std::fs::File;
+use std::io::prelude::*;
+use argparse::{ArgumentParser, Store};
+use failure::Error;
+use exitfailure::ExitFailure;
+
+
+pub mod vb_prog {
+    use hidapi;
+
+    pub struct FlashBoy {
+        dev : hidapi::HidDevice,
+    }
+
+    impl FlashBoy {
+        pub fn open() -> Result<FlashBoy, failure::Error> {
+            let api = hidapi::HidApi::new()?;
+            let device = match api.open(0x1781, 0x09a2) {
+                Ok(x) => { x },
+                Err(_) => { return Err(From::from(Error::FlashboyNotFound)) }
+            };
+
+            // Plenty of non-FlashBoy devices use Atmel micros, so check for string.
+            if !device.get_product_string()?
+                      .ok_or(Error::FlashboyNotFound)?
+                      .contains("FlashBoy") {
+                return Err(From::from(Error::FlashboyNotFound));
+            }
+
+            Ok(FlashBoy {
+                dev : device,
+            })
+        }
+    }
+
+    #[derive(Debug, Fail)]
+    pub enum Error {
+        #[fail(display = "Could not find Flashboy Plus device")]
+        FlashboyNotFound,
+    }
+}
+
+
+use self::vb_prog::*;
+
+fn main() -> Result<(), ExitFailure> {
+    let mut rom = String::new();
+
+    {
+        let mut ap = ArgumentParser::new();
+        ap.set_description("Command-line Virtual Boy Flash Programmer");
+        ap.refer(&mut rom)
+            .add_argument("rom", Store, "Virtual Boy ROM image to flash.")
+            .required();
+        ap.parse_args_or_exit();
+    }
+
+    let flash = FlashBoy::open()?;
+
+    Ok(())
 }
